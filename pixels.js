fd0074928b5263cc9414064bc69335c01dd0dad6
Vals098 <valeria.farinosi@gmail.com>
2026-04-16 16:28:57 +0200

    add: image change at button click

---
+++ pixels.js
@@ -0,0 +1,75 @@
+const API_KEY = "WgEAmxpIXv4ZVN2DhzZaBO26HgL6rAvy7UIgo1rTMRsLlKDT59vucRja"
+
+
+// FIRST FETCH: HAMSTERS
+const getImages = function () {
+  fetch("https://api.pexels.com/v1/search?query=hamsters", {
+    method: "GET",
+    headers: {
+      Authorization: API_KEY,
+    },
+  })
+    .then((response) => {
+      console.log("RESPONSE", response)
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error("incorrect server response", response.status)
+      }
+    })
+    .then((hamsters) => {
+      console.log("HAMSTERS IMG", hamsters)
+      // get cards
+      const cards = document.querySelectorAll(".card")
+
+      hamsters.photos.forEach((photo, i) => {
+        const card = cards[i]
+
+        const img = card.querySelector("img")
+        // decide the dimension of the image by .large .medium .small . portrait .landscape ...
+        img.src = photo.src.medium
+      })
+    })
+    .catch((error) => {
+      console.log(`fetch error: ${error}`)
+    })
+}
+
+document.getElementById("loadImages").addEventListener("click", getImages)
+
+
+// SECOND FETCH: TIGERS
+const getSecondaryImages = function () {
+  fetch("https://api.pexels.com/v1/search?query=tigers", {
+    method: "GET",
+    headers: {
+      Authorization: API_KEY,
+    },
+  })
+    .then((response) => {
+      console.log("RESPONSE", response)
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error("incorrect server response", response.status)
+      }
+    })
+    .then((tigers) => {
+      console.log("TIGERS IMG", tigers)
+      // get cards
+      const cards = document.querySelectorAll(".card")
+
+      tigers.photos.forEach((photo, i) => {
+        const card = cards[i]
+
+        const img = card.querySelector("img")
+        // decide the dimension of the image by .large .medium .small . portrait .landscape ...
+        img.src = photo.src.medium
+      })
+    })
+    .catch((error) => {
+      console.log(`fetch error: ${error}`)
+    })
+}
+
+document.getElementById("loadSecondaryImages").addEventListener("click", getSecondaryImages)
